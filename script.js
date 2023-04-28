dfe1556d75f94b85fdead01ac0e7c8457d8aba44
BurnettMiao <searchfor1987@gmail.com>
2023-04-28 10:21:50 +0800

    refresh project

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-// const apiKey = '使用openweather api練習';
-// const apiUrl = '使用openweather api練習';
+const apiKey = '720fad97132fa9c7cf0fc47af58ede18';
+const apiUrl =
+  'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
 
 const searchBox = document.querySelector('.search input');
 const searchBtn = document.querySelector('.search button');
